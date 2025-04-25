71e2545c731efd8e3ff4d03515e5cb65b1ef42cf
nimishkhadilkar <nimishkhadilkar7@gmail.com>
2025-04-25 18:09:06 +0530

    frontend changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+// const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://book-management-seven-lime.vercel.app/api';
 
 const apiClient = axios.create({
   baseURL: API_URL,
